81a83dc7c5260f92b056456f52c42c3ab9220beb
Shrey <macbook@Shrey.local>
2021-04-30 18:34:32 -0400

    QRCODE

--- index.js
+++ index.js
@@ -18,74 +18,8 @@ const { exception } = require('console');
 
 // // // Create a new express application instance
 
-// // app.use(bodyParser.urlencoded({ extended: true }));
-// // app.use(express.static("www"));
 
-// let oSockets = {};
-// let oOrders = {};
-// app.post("/payment", (req, res) => {
-//   // this happens when the order is complete
-//   sFrom = req.params.phone;
-//   const aReply = oOrders[sFrom].handleInput(req.body);
-//   const oSocket = oSockets[sFrom];
-//   // send messages out of turn
-//   for (let n = 0; n < aReply.length; n++) {
-//     if (oSocket) {
-//       const data = {
-//         message: aReply[n]
-//       };
-//       oSocket.emit('receive message', data);
-//     } else {
-//       throw new Exception("twilio code would go here");
-//     }
-//   }
-//   if (oOrders[sFrom].isDone()) {
-//     delete oOrders[sFrom];
-//     delete oSockets[sFrom];
-//   }
-//   res.end("ok");
-// });
 
-// app.get("/payment", (req, res) => {
-//   // this happens when the user clicks on the link in SMS
-//   const sFrom = req.params.phone;
-//   if (!oOrders.hasOwnProperty(sFrom)) {
-//     res.end("order already complete");
-//   } else {
-//     res.end(oOrders[sFrom].renderForm());
-//   }
-// });
-
-// app.post("/sms", (req, res) => {
-//   // turn taking SMS
-//   let sFrom = req.body.From || req.body.from;
-//   let sUrl = `${req.headers['x-forwarded-proto'] || req.protocol}://${req.headers['x-forwarded-host'] || req.headers.host}${req.baseUrl}`;
-//   if (!oOrders.hasOwnProperty(sFrom)) {
-//     oOrders[sFrom] = new PopupMeal(sFrom, sUrl);
-//   }
-//   if (oOrders[sFrom].isDone()) {
-//     delete oOrders[sFrom];
-//   }
-//   let sMessage = req.body.Body || req.body.body;
-//   let aReply = oOrders[sFrom].handleInput(sMessage);
-//   res.setHeader('content-type', 'text/xml');
-//   let sResponse = "<Response>";
-//   for (let n = 0; n < aReply.length; n++) {
-//     sResponse += "<Message>";
-//     sResponse += aReply[n];
-//     sResponse += "</Message>";
-//   }
-//   res.end(sResponse + "</Response>");
-// });
-
-// io.on('connection', function (socket) {
-//   // when the client emits 'receive message', this listens and executes
-//   socket.on('receive message', function (data) {
-//     // set up a socket to send messages to out of turn
-//     const sFrom = _.escape(data.from);
-//     oSockets[sFrom] = socket;
-//   });
-// });
 
 var paymentDetailReq = null;
 var phone = "";
@@ -151,7 +85,7 @@ app.post("/payment/", (req, res) => {
         // This function displays Smart Payment Buttons on your web page.
       </script>
       <p>After your payment, please click the QR Code link below:</p>
-      <a href="http://localhost:3002/QRCode">QR Code</a>
+      <a href="https://popup-meal-sms-chatbot.herokuapp.com/payment/YVP11NLZQ1/QRCode">QR Code</a>
     
     </body>
         
@@ -331,15 +265,6 @@ app.get("/qrcode/:phone",(req,res)=>{
     `);  
   }
   res.end(renderQRCode());
-
-  // console.log(oOrders); 
-  // console.log(req.body);
-  // const sFrom = req.params.phone;
-  // if (!oOrders.hasOwnProperty(sFrom)) {
-  //   res.end("order already complete");
-  // } else {
-  //   res.end(oOrders[sFrom].renderForm());
-  // }
 });
 
 app.post("/test",(req,res)=>{
